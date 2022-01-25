5c802a43fc024171b8ad9bfe0d7444ada4c7c482
Patxi <patxi.aguirre@hslu.ch>
2022-01-25 11:51:37 +0100

    stuff

--- gui/img_utils.py
+++ gui/img_utils.py
@@ -1,4 +1,5 @@
 from PySide2 import QtCore, QtGui
+from PySide2.QtWidgets import *
 
 
 def iconFromBase64(base64):
@@ -6,3 +7,6 @@ def iconFromBase64(base64):
     pixmap.loadFromData(QtCore.QByteArray.fromBase64(base64))
     icon = QtGui.QIcon(pixmap)
     return icon
+
+def ToggleIconAction(action: QAction, icon: QtGui.QIcon):
+    action.setIcon(icon)
--- gui/main.py
+++ gui/main.py
@@ -5,6 +5,7 @@ from PySide2.QtGui import QIcon
 from PySide2.QtWidgets import *
 import os
 import sys
+import getpass
 
 from tray_menu import *
 from img_utils import *
@@ -12,9 +13,14 @@ from draw_items import *
 
 
 def main():
+    work_user = ["hslu","admin"]
     app = QApplication(sys.argv)
 
-    m_ico_b64 = iconFromBase64(menu_image)
+    if getpass.getuser() in work_user:
+        m_ico_b64 = iconFromBase64(bergli)
+    else:
+        m_ico_b64 = iconFromBase64(juche)
+
     m_ico = QIcon(m_ico_b64)
 
     cur_path = os.path.dirname(os.path.realpath(__file__))
@@ -26,6 +32,11 @@ def main():
 
     menu = PatxiMenu(software_files)
     menu.BuildMenu()
+
+    OCIO = QAction(m_ico,"OCIO")
+    OCIO.triggered.connect(lambda: ChangeIconAction(OCIO, bergli))
+
+    menu.addAction(OCIO)
 
     menu.addAction("Quit").triggered.connect(sys.exit)
 
