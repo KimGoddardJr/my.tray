36166d3a6f2c00bfeface6477feba97a697af817
Kim Goddard Jr <procedural.godart@gmail.com>
2022-02-02 15:01:21 +0100

    *

--- start-unix.sh
+++ start-unix.sh
@@ -1,5 +1,9 @@
-SCRIPT_PATH = $(pwd)
+#!/usr/bin/env bash
 
-conda activate vfxpipeline
+# eval "$(conda shell.bash hook)"
 
-python "${SCRIPT_PATH}/tray/main.py"
+source $HOME/anaconda3/bin/activate vfxpipeline
+
+SCRIPT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+python3 "${SCRIPT_PATH}/tray/main.py"
